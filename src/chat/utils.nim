367df226e990750575ac9ce8ddf4d777cec988d0
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2026-02-11 09:59:10 -0800

    Update library to work with Libchat

--- src/chat/utils.nim
+++ src/chat/utils.nim
@@ -6,7 +6,6 @@ import strutils
 proc getCurrentTimestamp*(): Timestamp =
     result = waku_core.getNanosecondTime(getTime().toUnix())
 
-
 proc hash_func*(s: string | seq[byte]): string =
     # This should be Blake2s but it does not exist so substituting with Blake2b
     result = getBlake2b(s, 4, "")
@@ -17,17 +16,3 @@ proc bytesToHex*[T](bytes: openarray[T], lowercase: bool = false): string =
     for b in bytes:
         let hex = b.toHex(2)
         result.add(if lowercase: hex.toLower() else: hex)
-
-proc toBytes*(s: string): seq[byte] =
-    result = cast[seq[byte]](s)
-
-proc toUtfString*(b: seq[byte]): string =
-    result = cast[string](b)
-
-macro panic*(reason: string): untyped =
-    result = quote do:
-        let pos = instantiationInfo()
-        echo `reason` & " ($1:$2)" % [
-          pos.filename, $pos.line]
-        echo "traceback:\n", getStackTrace()
-        quit(1)
